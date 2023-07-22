724374295503bf9a6ace80eae75512d3c3dc3756
minho65 <skt_ks@naver.com>
2023-07-22 16:15:37 +0900

    FIX - fixed NotFound, NotFound css

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -2,18 +2,8 @@ import React from "react";
 
 const NotFound = () => {
     return (
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            fontSize: 64,
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-          }}
-        >
-          404
+        <div className="NotFound">
+          <div>404</div>
         </div>
       );
 }
